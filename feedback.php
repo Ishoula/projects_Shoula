6a88676610de585562d889e0f33cd1c93caeb1cf
Ishoula <165474242+Ishoula@users.noreply.github.com>
2024-11-23 08:25:44 +0200

    Update feedback.php

--- feedback.php
+++ feedback.php
@@ -30,7 +30,7 @@ if($_SERVER["REQUEST_METHOD"]=="POST"){
         <tbody>
         <tr>
         <td>Names</td>
-        <td>$fname + $lname</td>
+        <td>$fname . $lname</td>
         </tr>
         <tr>
         <td>Date of birth</td>
